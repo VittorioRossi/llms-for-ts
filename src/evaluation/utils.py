367c62768c3f87adcb03a61fe566ea1a03be877d
VittorioRossi <rossi.vittori@gmail.com>
2024-05-29 11:09:24 +0200

    Evaluate fix

--- src/evaluation/utils.py
+++ src/evaluation/utils.py
@@ -13,6 +13,13 @@ def mean_absolute_percentage_error(y_true:float, y_pred:float) -> float:
     return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
 
 def evaluate(y_true:np.array, y_pred:np.array) -> dict:
+    # check that both have shape (n)
+    if y_true.shape != y_pred.shape:
+        y_true = y_true.reshape(-1)
+        y_pred = y_pred.reshape(-1)
+
+    assert y_true.shape == y_pred.shape, f'Shapes do not match: {y_true.shape} != {y_pred.shape}'
+
     return {
         'mae': mean_absolute_error(y_true, y_pred),
         'mse': mean_squared_error(y_true, y_pred),
