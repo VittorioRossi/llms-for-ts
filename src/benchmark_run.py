972c8228e6ecab26b9d436a1727be91593bf07b6
VittorioRossi <rossi.vittori@gmail.com>
2024-05-30 13:43:44 +0200

    save files

--- src/benchmark_run.py
+++ src/benchmark_run.py
@@ -60,8 +60,11 @@ def main(model_name, dataset_name, prompt_name, window_size, target_size, batch_
     preds = np.array(preds).reshape(-1, target_size)
     true = np.array(true).reshape(-1, target_size)
 
-    pd.DataFrame({"preds":preds, "true":true}).to_csv('preds.csv', index=False)
     
+    logger.info('Saving predictions and true values')
+    np.savetxt('predictions.txt', preds)
+    np.savetxt('true_values.txt', true)
+
     logger.info('Evaluating model')
     eval = evaluate(true, preds)
     logger.info(eval)
