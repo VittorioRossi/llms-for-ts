d67d40de155ee98bb8c95e0cc47467c9af522209
VittorioRossi <rossi.vittori@gmail.com>
2024-06-01 00:53:20 +0200

    fix bug

--- src/benchmark_run.py
+++ src/benchmark_run.py
@@ -61,7 +61,6 @@ def run_experiment(model_name, dataset_name, prompt_name, window_size, target_si
     n_batches = 0
     for observation in tqdm(data_generator, total=num_bateches):
         prediction = model.generate(observation[0])
-        print(prediction)
         preds.extend(prediction)
         true.extend(observation[1])
         
@@ -71,7 +70,8 @@ def run_experiment(model_name, dataset_name, prompt_name, window_size, target_si
 
         n_batches += 1
 
-
+    print(true)
+    print(preds)
     preds = np.array(preds).reshape(-1, target_size).astype(float)
     true = np.array(true).reshape(-1, target_size).astype(float)
 
