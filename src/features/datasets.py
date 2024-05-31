1c1f3b2b6feb1619eacdf04ab0725b05a4589531
VittorioRossi <rossi.vittori@gmail.com>
2024-06-01 01:00:08 +0200

    fix bug

--- src/features/datasets.py
+++ src/features/datasets.py
@@ -20,7 +20,7 @@ def load_cached_data(cache_path):
     with open(cache_path, 'r') as f:
         data = f.readlines()
     X, y = zip(*[line.strip().split(',') for line in data])
-    return X, y
+    return np.array(X, dtype=float), np.array(X, dtype=float)
 
 class Dataset(ABC):
     @abstractmethod
