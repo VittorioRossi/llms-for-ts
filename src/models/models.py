0f01370fb4676ad5a8d4061a097a9c0c88f2b39e
VittorioRossi <rossi.vittori@gmail.com>
2024-06-01 00:25:55 +0200

    refactoring model code

--- src/models/models.py
+++ src/models/models.py
@@ -3,6 +3,8 @@ from transformers import pipeline, set_seed, AutoModelForCausalLM, AutoTokenizer
 from abc import ABC, abstractmethod
 import os
 import numpy as np
+
+
 def set_pad_token_if_missing(tokenizer):
     if tokenizer.pad_token is None:
         tokenizer.add_special_tokens({'pad_token': '[PAD]'})
@@ -14,7 +16,6 @@ def compute_new_tokens(target_size, example_output, tokenizer):
     original_input_length = len(original_input_ids)
 
     return original_input_length*target_size
-
 
 def clean_pred(pred:str, target_size:int):
     # Transform the predicted tokens in a float
@@ -39,91 +40,92 @@ class LLM(ABC):
     def generate(self, prompt: str) -> str:
         pass
 
-class HuggingFaceLLM(LLM):
-    def __init__(self, model: str, example_output="00.0"):
-        # Detect if CUDA is available and set the device accordingly
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        try:
-            self.generator = self.setup_generator(model, example_output=example_output)
-        except Exception as e:
-            # Setup the pipeline if no exceptions
-            self.generator = pipeline('text-generation', model=self.model, tokenizer=self.tokenizer, device=0 if torch.cuda.is_available() else -1)
+import os
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
-    def setup_generator(self, model_name, target_size=1, example_output="00.0"):
+class HuggingFaceLLM(LLM):
+    def __init__(self, model: str, example_output="00.0", target_size=1):
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.generator = self.setup_generator(model, example_output, target_size)
+
+    def setup_generator(self, model_name, example_output="00.0", target_size=1):
         token = os.environ.get("HUGGINGFACE_TOKEN")
-        model = AutoModelForCausalLM.from_pretrained(model_name,
-                                                     cache_dir="models",
-                                                     torch_dtype="auto",
-                                                     use_auth_token=token).to(self.device)
-        tokenizer_kwargs = {}
-        if model_name.__contains__('bert'):
-            tokenizer_kwargs['padding_side'] = 'left'
-        
-        tokenizer = AutoTokenizer.from_pretrained(model_name, use_auth_token=token, **tokenizer_kwargs)
+        model = self.load_model(model_name, token)
+        tokenizer = self.load_tokenizer(model_name, token)
 
         tokenizer = set_pad_token_if_missing(tokenizer)
-        
         max_new_tok = compute_new_tokens(target_size, example_output, tokenizer)
 
-        def gen(texts, max_new_tokens=None):
-            # Ensure the tokenizer has a pad token
-            if max_new_tokens is None:
-                max_new_tokens = max_new_tok
-            
-            # Tokenize the input texts with padding and truncation enabled
-            inputs = tokenizer(
-                texts,
-                return_tensors="pt",
-                padding=True,
-                truncation=True)
-            
-            # Move the inputs to the appropriate device (CUDA or CPU)
-            device = 'cuda' if torch.cuda.is_available() else 'cpu'
-            model.to(device)
-            inputs = {k: v.to(device) for k, v in inputs.items()}
-            
-            # Generate new tokens using the model
-            outputs = model.generate(
-                input_ids=inputs['input_ids'],
-                attention_mask=inputs['attention_mask'],
-                max_new_tokens=max_new_tokens,
-                pad_token_id=tokenizer.pad_token_id  # Ensure proper padding if needed
-            )
-            
-            # Decode the generated tokens to text, skipping special tokens
-            generated_texts = tokenizer.batch_decode(outputs, skip_special_tokens=True)
-            
-            # Clean up by deleting unnecessary variables and clearing cache
-            del inputs
-            del outputs
-            torch.cuda.empty_cache()
-            
-            results = []
-            for text, generated_text in zip(texts, generated_texts):
-                # Tokenize the original input text to find its length
-                original_input_ids = tokenizer(text, add_special_tokens=False)['input_ids']
-                original_input_length = len(original_input_ids)
-
-                # Tokenize the full generated text to find the portion that corresponds to the prediction
-                full_generated_ids = tokenizer(generated_text, add_special_tokens=False)['input_ids']
-
-                # Get the part of the generated text that corresponds to the prediction
-                new_token_ids = full_generated_ids[original_input_length:]
-
-                # Decode the new tokens to get the prediction text
-                prediction = tokenizer.decode(new_token_ids, skip_special_tokens=True)
-
-                preds = clean_pred(prediction, target_size)
-                results.append(preds)
-
+        def gen(texts, max_new_tokens=max_new_tok):
+            inputs = self.tokenize_inputs(tokenizer, texts)
+            outputs = self.generate_outputs(model, tokenizer, inputs, max_new_tokens)
+            results = self.decode_outputs(tokenizer, texts, outputs, target_size=target_size)
             return results
-    
-        return gen 
 
-    def generate(self, prompt: str, max_new_tokens=None) -> str:
-        if hasattr(self, 'generator'):
-            return self.generator(prompt, max_new_tokens=max_new_tokens)
-        else:
-            inputs = self.tokenizer(prompt, return_tensors="pt").to(self.device)
-            outputs = self.model.generate(**inputs, max_new_tokens=max_new_tokens)
-            return self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+        return gen
+
+    def load_model(self, model_name, token):
+        return AutoModelForCausalLM.from_pretrained(
+            model_name,
+            cache_dir="models",
+            torch_dtype="auto",
+            use_auth_token=token
+        ).to(self.device)
+
+    def load_tokenizer(self, model_name, token):
+        tokenizer_kwargs = {}
+        if 'bert' in model_name:
+            tokenizer_kwargs['padding_side'] = 'left'
+        return AutoTokenizer.from_pretrained(model_name, use_auth_token=token, **tokenizer_kwargs)
+
+    def tokenize_inputs(self, tokenizer, texts):
+        return tokenizer(
+            texts,
+            return_tensors="pt",
+            padding=True,
+            truncation=True
+        ).to(self.device)
+
+    def generate_outputs(self, model, tokenizer, inputs, max_new_tokens):
+        model.to(self.device)
+        inputs = {k: v.to(self.device) for k, v in inputs.items()}
+        return model.generate(
+            input_ids=inputs['input_ids'],
+            attention_mask=inputs['attention_mask'],
+            max_new_tokens=max_new_tokens,
+            pad_token_id=tokenizer.pad_token_id
+        )
+
+    def decode_outputs(self, tokenizer, texts, outputs, target_size):
+        generated_texts = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+        torch.cuda.empty_cache()
+
+        original_lengths = [len(tokenizer(text, add_special_tokens=False)['input_ids']) for text in texts]
+        new_texts = [generated_text[len(text):] for generated_text, text in zip(generated_texts, texts)]
+
+        predictions = [
+            tokenizer.decode(
+                tokenizer(new_text, add_special_tokens=False)['input_ids'][original_length:],
+                skip_special_tokens=True
+            )
+            for new_text, original_length in zip(new_texts, original_lengths)
+        ]
+
+        return [clean_pred(prediction, target_size) for prediction in predictions]
+
+def set_pad_token_if_missing(tokenizer):
+    if tokenizer.pad_token is None:
+        tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+    return tokenizer
+
+def compute_new_tokens(target_size, example_output, tokenizer):
+    example_tokens = tokenizer(example_output, add_special_tokens=False)['input_ids']
+    return target_size * len(example_tokens)
+
+def clean_pred(pred: str, target_size: int):
+    # Split the predicted tokens and take only the first `target_size` elements
+    tokens = pred.split(" ")[:target_size]
+    
+    # Convert tokens to floats, using np.nan for any conversion errors
+    return [float(token) if token.replace('.', '', 1).isdigit() else np.nan for token in tokens]
