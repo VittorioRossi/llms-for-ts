c67b85d9f272b357b95918d7f4e6e29e50848b03
VittorioRossi <rossi.vittori@gmail.com>
2024-06-02 12:21:20 +0200

    edit splitting in run baseline

--- src/run_baseline.py
+++ src/run_baseline.py
@@ -55,7 +55,7 @@ def run_experiment(model_name, dataset_name, window_size, target_size, batch_siz
     num_bateches = limit_obs//batch_size
     n_batches = 0
     for observation in tqdm(data_generator, total=num_bateches):
-        cleaned_obs = [list(map(float, obs.split())) for obs in observation[0]]
+        cleaned_obs = [list(map(float, obs.split(" "))) for obs in observation[0]]
         prediction = [model(cl, target_size) for cl in cleaned_obs]
 
         preds.extend(prediction)
