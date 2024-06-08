17462b4404c3c63a2c4bc4e20d05bfe7376bb919
VittorioRossi <rossi.vittori@gmail.com>
2024-06-08 17:17:22 +0200

    FIX: adjust sas for multi step

--- src/models/baselines.py
+++ src/models/baselines.py
@@ -17,14 +17,13 @@ def mean_forecast(data, target_size=1, seasonality=0):
     return [avg for _ in range(target_size)] 
 
 def ses_forecast(data, target_size=1, seasonality=0):
-    if seasonality > 0:
-        # Initialize the Exponential Smoothing model with seasonality
-        model = ExponentialSmoothing(
-            data,
-            seasonal='add',  # or 'mul' for multiplicative seasonality
-            seasonal_periods=seasonality,
-            initialization_method='estimated'
-        ).fit()
+    # Initialize the Exponential Smoothing model with seasonality
+    model = ExponentialSmoothing(
+        data,
+        seasonal='add',  # or 'mul' for multiplicative seasonality
+        seasonal_periods=seasonality,
+        initialization_method='estimated'
+    ).fit()
 
     # Generate the forecast for the given target size
     forecast = model.forecast(target_size)
