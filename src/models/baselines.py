b826e370e32f8d0e1cb314904bfc7b8e8c5d8528
VittorioRossi <rossi.vittori@gmail.com>
2024-06-08 12:40:55 +0200

    FIX: fix mean predict for multiple target

--- src/models/baselines.py
+++ src/models/baselines.py
@@ -14,7 +14,7 @@ def naive_forecast(data, target_size=1, seasonality=0):
     return data[-seasonality:-seasonality+target_size]
 
 def mean_forecast(data, target_size=1, seasonality=0):
-    return [np.mean(data)]
+    return [np.mean(data) for _ in range(target_size)] 
 
 def ses_forecast(data, target_size=1, seasonality=0):
     model = ExponentialSmoothing(data, 
