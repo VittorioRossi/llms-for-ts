4f528dea6eecb60d171bb5f908424a3e4060a15d
VittorioRossi <rossi.vittori@gmail.com>
2024-06-12 11:04:11 +0200

    adjust loading cached dataset

--- src/features/datasets.py
+++ src/features/datasets.py
@@ -23,7 +23,7 @@ def load_cached_data(cache_path, batch_size=64):
     with open(cache_path, 'r') as f:
         data = f.readlines()
 
-    X, y = zip(*[line.strip().split(';') for line in data])
+    X, y = zip(*[line.replace('\'', '').strip().split(';') for line in data])
     X = list(X)
 
     y = [ast.literal_eval(item) for item in y]
